f41be842fc5c98d33de8a04596633904cadf7d6a
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-21 21:40:22 +0000

    link db to routes

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ const category = require('./routes/category');
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/register", register());
-app.use('/login', login());
-app.use('/category', category());
+app.use("/register", register(db));
+app.use('/login', login(db));
+app.use('/category', category(db));
 // Note: mount other resources here, using the same pattern above
 
 
