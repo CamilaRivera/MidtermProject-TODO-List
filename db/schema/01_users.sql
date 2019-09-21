f664ea664cec326e03f45e0bed1f0b3e0752c3db
CamilaRivera <camila.riverart@gmail.com>
2019-09-21 20:17:35 +0000

    Database migrations and seeds

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,12 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  avatar_url VARCHAR(255) NOT NULL,
+  birth_date DATE NOT NULL
 );
+
---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,8 @@
+-- Widgets table seeds here (Example)
+INSERT INTO categories (id, description, creation_date, user_id, cover_photo_url)
+VALUES (1, 'Books', '2018-11-02', 1, 'https://images.pexels.com/photos/2104151/pexels-photo-2104151.jpeg'),
+(2, 'Movies', '2017-11-02', 1, 'https://images.pexels.com/photos/2104146/pexels-photo-2104146.jpeg'),
+(3, 'Sports', '2019-02-02', 2, 'https://images.pexels.com/photos/63590/san-francisco-california-city-cities-63590.jpeg'),
+(4, 'Restaurants', '2019-03-03', 3, 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350');
+
+
