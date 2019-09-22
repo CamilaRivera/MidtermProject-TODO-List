10231cbc3bd44f35c9eee71f45c55883c8568a5f
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-22 02:50:26 +0000

    include name, avatar, password, email, and date options for register

--- routes/register.js
+++ routes/register.js
@@ -7,7 +7,11 @@ module.exports = (db) => {
   });
   
   router.post('/', (req, res) => {
-  
+    const {userName, email, password} = req.body;
+    const values = [userName, email, password];
+    const queryString = `
+      INSERT INTO users (name, email, phone_number, password, avatar_url, birth_date)
+    `;
   });
 
   return router;
