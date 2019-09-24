bcac42ab219e9337c30b12b70bb6e2307881c81d
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-24 06:14:22 +0000

    const taskName = $(#todo_title); :q w

--- public/scripts/categorize.js
+++ public/scripts/categorize.js
@@ -1,15 +1,14 @@
 $(() => {
   const taskName = $('#todo_title');
   const categoryFiller = $('#category_selection');
-  taskName.keypress((event) => {
-    if (event.keyCode === 13) {
-      $.ajax('/api/categories/sort', {
-        method: "POST",
-        data: taskName.serialize()
-      }).then(cateogry => {
-        categoryFiller.focus();
-        categoryFiller.val(cateogry);
-      });
-    }
+  taskName.on('keyup', () => {
+    $.ajax('/api/categories/sort', {
+      method: "POST",
+      data: taskName.serialize()
+    }).then(cateogry => {
+      categoryFiller.focus();
+      categoryFiller.val(cateogry);
+      taskName.focus();
+    });
   });
 });
