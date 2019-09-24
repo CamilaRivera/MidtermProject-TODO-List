e1a62f87c0719f3b53928c565979cf46c12742dd
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-24 02:17:23 +0000

    change slider item to card reveal, move taskFunction.js to routes/helpers

--- public/scripts/viewTask.js
+++ public/scripts/viewTask.js
@@ -6,10 +6,29 @@ $(() => {
       mainConatiner.html(`<div class="carousel"></div>`);
       const slider = $('.carousel');
       const {data} = list;
-      data.forEach((task, index) => {
-        slider.append(`<a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>`);
+      console.log(data);
+      data.forEach((task) => {
+        slider.append(`
+        <div class="row carousel-item">
+        <div class="col s12 m12">
+        <div class="card">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="https://lorempixel.com/250/250/nature/1">
+        </div>
+        <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">${task.title}<i class="material-icons right">more_vert</i></span>
+        </div>
+        <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${task.title}<i class="material-icons right">close</i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        </div>
+        </div>
+        </div>
+      </div>
+        `);
       });
-      slider.carousel(); //initiate slider
+      slider.carousel({
+      });
     });
 });
 
