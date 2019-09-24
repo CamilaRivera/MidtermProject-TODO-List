cbc0eae1c38bc06f1357ac9d97aaa56dc9a7d1bd
Zongxi Li <zongxiliwork@gmail.com>
2019-09-24 16:20:15 +0000

    comment the viewTask.js

--- public/scripts/viewTask.js
+++ public/scripts/viewTask.js
@@ -1,55 +1,55 @@
-$(() => {
+// $(() => {
 
-  $.ajax('api/categories/1', { method: 'GET' }) //where 1 is has to be dynamic
-    .then(list => {
-      const mainConatiner = $('main');
-      mainConatiner.css('padding-left', '25vw');
-      mainConatiner.css('transform', 'translateY(-30vh)');
-      mainConatiner.html(`<div class="carousel" style="
-        width: 100vw;
-        height: 100vh;
-        overflow: visible !important;
-      "></div>
-        <p>
-          Hi Max, remember you wanted to watch these movies?
-        </p>
-      `);
-      const slider = $('.carousel');
-      const { data } = list;
-      const moviePromise = [];
-      data.forEach((task) => {
-        moviePromise.push($.ajax('api/widgets/movie', {method: 'POST', data: task.title}));
-      });
-      Promise.all(moviePromise)
-        .then(movies => {
-          console.log(movies);
-          movies.forEach(movie => {
-            slider.append(`
-        <div class="row carousel-item">
-        <div class="col s12 m12">
-        <div class="card" style="
-            width:20vw;
-        ">
-        <div class="card-image waves-effect waves-block waves-light" style="
-            width: 20vw;
-        ">
-          <img class="activator" src="${movie.Poster}">
-        </div>
-        <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">${movie.Title}<i class="material-icons right">more_vert</i></span>
-        </div>
-        </div>
-        </div>
-      </div>
-        `);
-          });
-          slider.carousel();
-        });
+//   $.ajax('api/categories/1', { method: 'GET' }) //where 1 is has to be dynamic
+//     .then(list => {
+//       const mainConatiner = $('main');
+//       mainConatiner.css('padding-left', '25vw');
+//       mainConatiner.css('transform', 'translateY(-30vh)');
+//       mainConatiner.html(`<div class="carousel" style="
+//         width: 100vw;
+//         height: 100vh;
+//         overflow: visible !important;
+//       "></div>
+//         <p>
+//           Hi Max, remember you wanted to watch these movies?
+//         </p>
+//       `);
+//       const slider = $('.carousel');
+//       const { data } = list;
+//       const moviePromise = [];
+//       data.forEach((task) => {
+//         moviePromise.push($.ajax('api/widgets/movie', {method: 'POST', data: task.title}));
+//       });
+//       Promise.all(moviePromise)
+//         .then(movies => {
+//           console.log(movies);
+//           movies.forEach(movie => {
+//             slider.append(`
+//         <div class="row carousel-item">
+//         <div class="col s12 m12">
+//         <div class="card" style="
+//             width:20vw;
+//         ">
+//         <div class="card-image waves-effect waves-block waves-light" style="
+//             width: 20vw;
+//         ">
+//           <img class="activator" src="${movie.Poster}">
+//         </div>
+//         <div class="card-content">
+//       <span class="card-title activator grey-text text-darken-4">${movie.Title}<i class="material-icons right">more_vert</i></span>
+//         </div>
+//         </div>
+//         </div>
+//       </div>
+//         `);
+//           });
+//           slider.carousel();
+//         });
         
-      // slider.carousel({
-      // });
-    });
-});
+//       // slider.carousel({
+//       // });
+//     });
+// });
 
 // <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
 // <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
