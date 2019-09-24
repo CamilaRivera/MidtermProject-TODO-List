d37dcabfb1507539747135a06ab5df8c046ea4be
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-24 03:52:04 +0000

    complete basic slider view

--- public/scripts/viewTask.js
+++ public/scripts/viewTask.js
@@ -5,30 +5,39 @@ $(() => {
       const mainConatiner = $('main');
       mainConatiner.html(`<div class="carousel"></div>`);
       const slider = $('.carousel');
-      const {data} = list;
-      console.log(data);
+      const { data } = list;
+      const moviePromise = [];
       data.forEach((task) => {
-        slider.append(`
+        moviePromise.push($.ajax('api/widgets/movie', {method: 'POST', data: task.title}));
+      });
+      Promise.all(moviePromise)
+        .then(movies => {
+          console.log(movies);
+          movies.forEach(movie => {
+            slider.append(`
         <div class="row carousel-item">
         <div class="col s12 m12">
         <div class="card">
         <div class="card-image waves-effect waves-block waves-light">
-          <img class="activator" src="https://lorempixel.com/250/250/nature/1">
+          <img class="activator" src="${movie.Poster}">
         </div>
         <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">${task.title}<i class="material-icons right">more_vert</i></span>
+      <span class="card-title activator grey-text text-darken-4">${movie.Title}<i class="material-icons right">more_vert</i></span>
         </div>
         <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">${task.title}<i class="material-icons right">close</i></span>
+        <span class="card-title grey-text text-darken-4">${movie.Title}<i class="material-icons right">close</i></span>
           <p>Here is some more information about this product that is only revealed once clicked on.</p>
         </div>
         </div>
         </div>
       </div>
         `);
-      });
-      slider.carousel({
-      });
+          });
+          slider.carousel();
+        });
+        
+      // slider.carousel({
+      // });
     });
 });
 
