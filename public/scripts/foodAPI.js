7abd4609ea86579ace324ec267c80c30a39fea3b
CamilaRivera <camila.riverart@gmail.com>
2019-09-25 06:22:31 +0000

    Refactoring movies and food

--- public/scripts/foodAPI.js
+++ public/scripts/foodAPI.js
@@ -1,76 +1,44 @@
 $(() => {
 
   $('.eat-todos').on('click', () => {
-    $.ajax('api/categories/4', { method: 'GET' }) //where 1 is has to be dynamic
-      .then(list => {
-        console.log(list);
-        const mainConatiner = $('main');
-        mainConatiner.css('display', 'flex');
-        mainConatiner.css('padding-left', '25vw');
-        mainConatiner.css('transform', 'translateY(-30vh)');
-        mainConatiner.html(`<div class="carousel" style="
-          width: 100vw;
-          height: 100vh;
-          overflow: visible !important;
-        ">
-        `);
-        const slider = $('.carousel');
-        const { data } = list;
-        const foodPromise = [];
-        data.forEach((task) => {
-          foodPromise.push($.ajax('api/widgets/food', { method: 'POST', data: task.title }));
-        });
-        Promise.all(foodPromise)
-          .then(food => {
-            console.log(food);
-            mainConatiner.prepend(`
-          <div class="task-info" style="
-          margin-top: 35vh;
-          margin-left: -2vw;
-          width: 100vw;
-          ">
-          </div>
-            `);
-            food.forEach(food => {
-              slider.append(`
+    const list = todos.filter(todo => todo.category_id === 4);
+    renderTodos(list);
+    console.log(list);
+    const slider = $('.carousel');
+    slider.empty();
+    const foodPromise = [];
+    list.forEach((todo) => {
+      foodPromise.push($.ajax('api/widgets/food', { method: 'POST', data: todo.title }));
+    });
+    Promise.all(foodPromise)
+      .then(foods => {
+        console.log(foods);
+        foods.forEach(food => {
+          slider.append(`
           <div class="row carousel-item">
-          <div class="col s12 m12">
-          <div class="card" style="
-              width:20vw;
-          ">
-          <div class="card-image waves-effect waves-block waves-light" style="
-              width: 20vw;
-          ">
-            <img class="activator" src="${food.image}">
+            <div class="col s12 m12">
+              <div class="card food" style="width:20vw;">
+                <div class="card-image waves-effect waves-block waves-light" style="width: 20vw;">
+                  <img class="activator" src="${food.image}">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator grey-text text-darken-4" style="text-align: center"><p class="food-title">${food.name}</p><i class="material-icons left" style="transform: translateY(-100%);">check_circle</i><i class="material-icons right" style="transform: translateY(-100%);">cancel</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                  <p class="hidden-card-content">Here is some more information about this product that is only revealed once clicked on.</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4" style="text-align: center"><p class="food-title">${food.name}</p><i class="material-icons left" style="transform: translateY(-100%);">check_circle</i><i class="material-icons right" style="transform: translateY(-100%);">cancel</i></span>
-          </div>
-          <div class="card-reveal">
-          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-          <p class="hidden-card-content">Here is some more information about this product that is only revealed once clicked on.</p>
-          </div>
-          </div>
-          </div>
-        </div>
           `);
-              //   movieInfo.html(`
-              //   ${food.Title}
-              // `);
-            });
-            reloadAll()
-              .then((array) => {
-                const categoryTodos = array[1].filter(todo => 4 === todo.category_id);
-                $('.task-info').html(`
-                  ${makeTaskListHTML(categoryTodos)};
-                `);
-              });
-            slider.carousel({
-              onCycleTo: function (data) {
-                const currentFood = $(data).find('.food-title').html();
-                $.ajax('api/widgets/foodInfo', { method: 'POST', data: currentFood })
-                  .then(foodInfo => {
-                    $('.hidden-card-content').html(`
+        });
+        slider.carousel({
+          onCycleTo: function (data) {
+            const currentFood = $(data).find('.food-title').html();
+            $.ajax('api/widgets/foodInfo', { method: 'POST', data: currentFood })
+              .then(foodInfo => {
+                $('.hidden-card-content').html(`
                     <h5 class="genre">Price ${foodInfo.price}</h3>
                     <h5 class="type">Distance: ${foodInfo.distance} m</h3>
                     <h5 class="plot">Open: ${foodInfo.open}</h4>
@@ -78,10 +46,9 @@ $(() => {
                     <h5 class="rating">Rating: ${generateStars(foodInfo.rating, 5)}</h5>
                     <h5 class="rating-count">Number of Reviews: ${foodInfo.reviewCount}</h5>
                     `);
-                  });
-              }
-            });
-          });
+              });
+          }
+        });
       });
   });
 });
--- public/scripts/movieAPI.js
+++ public/scripts/movieAPI.js
@@ -1,44 +1,38 @@
 $(() => {
 
   $('.watch-todos').on('click', () => {
-    $.ajax('api/categories/1', { method: 'GET' }) //where 1 is has to be dynamic
-      .then(list => {
-        const mainConatiner = $('main');
-        mainConatiner.css('display', 'flex');
-        mainConatiner.css('padding-left', '25vw');
-        mainConatiner.css('transform', 'translateY(-30vh)');
-        mainConatiner.html(`<div class="carousel" style="
-          width: 100vw;
-          height: 100vh;
-          overflow: visible !important;
-        ">
-        `);
+    // $.ajax('api/categories/1', { method: 'GET' }) //where 1 is has to be dynamic
+    //   .then(list => {
+        const list = todos.filter(todo => todo.category_id === 1);
+        renderTodos(list);
+        console.log(list);
         const slider = $('.carousel');
-        const { data } = list;
+        slider.empty();
+    // const foodPromise = [];
+
+        // const mainConatiner = $('.carousel-container');
+        // const slider = $('.carousel');
         const moviePromise = [];
-        data.forEach((task) => {
+        list.forEach((task) => {
           moviePromise.push($.ajax('api/widgets/movie', { method: 'POST', data: task.title }));
         });
         Promise.all(moviePromise)
           .then(movies => {
-            mainConatiner.prepend(`
-          <div class="task-info" style="
-          margin-top: 35vh;
-          margin-left: -2vw;
-          width: 100vw;
-          ">
-          </div>
-            `);
+          //   mainConatiner.prepend(`
+          // <div class="task-info" style="
+          // margin-top: 35vh;
+          // margin-left: -2vw;
+          // width: 100vw;
+          // ">
+          // <section class='todos'>
+          // </div>
+          //   `);
             movies.forEach(movie => {
               slider.append(`
           <div class="row carousel-item">
           <div class="col s12 m12">
-          <div class="card" style="
-              width:20vw;
-          ">
-          <div class="card-image waves-effect waves-block waves-light" style="
-              width: 20vw;
-          ">
+          <div class="card movie">
+          <div class="card-image waves-effect waves-block waves-light">
             <img class="activator" src="${movie.Poster}">
           </div>
           <div class="card-content">
@@ -56,13 +50,12 @@ $(() => {
               //   ${movie.Title}
               // `);
             });
-            reloadAll()
-              .then((array) => {
-                const categoryTodos = array[1].filter(todo => 1 === todo.category_id);
-                $('.task-info').html(`
-                  ${makeTaskListHTML(categoryTodos)};
-                `);
-              });
+
+  //           reloadAll()
+  //             .then((array) => {
+  //               const categoryTodos = array[1].filter(todo => 1 === todo.category_id);
+  //               renderTodos(categoryTodos);
+  //             });
             slider.carousel({
               onCycleTo: function (data) {
                 const currentMovie = $(data).find('.movie-title').html();
@@ -81,7 +74,7 @@ $(() => {
           });
       });
   });
-});
+
 
 // <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
 // <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
